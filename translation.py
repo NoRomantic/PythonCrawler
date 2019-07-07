271f20410b14852e9eb649c40cfccafb6002d47c
wqz <1286730571@aa.com>
2019-07-07 23:04:17 +0800

    添加了headers

--- translation.py
+++ translation.py
@@ -1,32 +1,46 @@
 import urllib.request
 import urllib.parse
 import json
+import time
 
-content = input('请输入要翻译的词语：')
+while True:
+    content = input('请输入要翻译的词语(输入"q!"退出程序)：')
+    if content == 'q!':
+        break
+    
+    url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
 
-url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
-data = {}
+    '''
+    head = {}
+    head['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+    '''
 
-data['i']=content
-data['doctype']='json'
-data['keyfrom']='fanyi.web'
-data['typoResult']='true'
-data['from']='AUTO'
-data['to']='AUTO'
-data['smartresult']='dict'
-data['client']='fanyideskweb'
-data['salt']='1520416292076'
-data['sign']='41fe7ea28425a0a4ceb88ab4c8609d13'
-data['version']='2.1'
+    data = {}
+    data['i']=content
+    data['doctype']='json'
+    data['keyfrom']='fanyi.web'
+    data['typoResult']='true'
+    data['from']='AUTO'
+    data['to']='AUTO'
+    data['smartresult']='dict'
+    data['client']='fanyideskweb'
+    data['salt']='1520416292076'
+    data['sign']='41fe7ea28425a0a4ceb88ab4c8609d13'
+    data['version']='2.1'
+    data['action']='FY_BY_CLICKBUTTION'
+    data['typoResult']='false'
+    data = urllib.parse.urlencode(data).encode('utf-8')
 
-data['action']='FY_BY_CLICKBUTTION'
-data['typoResult']='false'
+    req = urllib.request.Request(url, data)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36')
 
-data = urllib.parse.urlencode(data).encode('utf-8')
+    response = urllib.request.urlopen(url, data)
+    html = response.read().decode('utf-8')
 
-response = urllib.request.urlopen(url, data)
-html = response.read().decode('utf-8')
+    ta = json.loads(html)
+    # print(html)
+    print(ta['translateResult'][0][0]['tgt'])
 
-ta = json.loads(html)
-print(html)
-print(ta['translateResult'][0][0]['tgt'])
+    time.sleep(5)
+
+
